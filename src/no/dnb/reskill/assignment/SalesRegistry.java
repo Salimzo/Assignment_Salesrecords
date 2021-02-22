9f9f208f6c882f45d95f9d5684d26dbeda36d62d
ZiggyHowland <sigbjorn@hovland.nu>
2021-02-22 12:51:17 +0100

    Added Sale class and function to create a Sale object in the SalesRegistry

--- src/no/dnb/reskill/assignment/SalesRegistry.java
+++ src/no/dnb/reskill/assignment/SalesRegistry.java
@@ -1,4 +1,16 @@
 package no.dnb.reskill.assignment;
 
+import java.util.ArrayList;
+
 public class SalesRegistry {
+    private ArrayList<Sale> sales = new ArrayList<>();
+
+
+    public static Sale createSaleFromCSVLine(String[] csvLineValues) {
+        Sale s = new Sale();
+        //TODO: Implement logic to parse from string values from CSV to Sale-attributes
+        return s;
+    }
+
+
 }
--- test/no/dnb/reskill/assignment/SalesRegistryTest.java
+++ test/no/dnb/reskill/assignment/SalesRegistryTest.java
@@ -1,7 +1,14 @@
 package no.dnb.reskill.assignment;
 
+import no.dnb.reskill.assignment.SalesRegistry;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SalesRegistryTest {
+    @Test
+    public void creatingSale_fromCSVLine_shouldReturnSaleObject() {
+        SalesRegistry fixture = new SalesRegistry();
+        fixture
+    }
 
 }
