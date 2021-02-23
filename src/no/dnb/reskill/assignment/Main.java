48c13f39abf4e1b427b2d181b7694f9fd12bffce
Salim Zouhri <salim.zouhri@outlook.com>
2021-02-23 12:44:58 +0100

    Pull

---
+++ src/no/dnb/reskill/assignment/Main.java
@@ -0,0 +1,22 @@
+package no.dnb.reskill.assignment;
+
+
+
+import java.util.Scanner;
+
+public class Main implements Usable {
+
+    SalesRegistry sr = new SalesRegistry(//legge inn filnavn og Salessregistry, her kan alle salgene oppbevares);
+
+
+    System.out.println("Enter CSV.file");
+    Scanner scanner = new Scanner(System.in);
+    scanner.nextLine();
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/no/dnb/reskill/assignment/Usable.java
@@ -0,0 +1,4 @@
+package no.dnb.reskill.assignment;
+
+public interface Usable {
+}
