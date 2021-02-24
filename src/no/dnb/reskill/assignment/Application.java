dd32ac006cb552dfef3ce1609b2eb954879ad211
marinahaugen <marina.santos.haugen@dnb.no>
2021-02-24 11:55:50 +0100

    Cleaned code in Application.java

--- src/no/dnb/reskill/assignment/Application.java
+++ src/no/dnb/reskill/assignment/Application.java
@@ -1,44 +1,28 @@
 package no.dnb.reskill.assignment;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.io.IOException;
-import java.util.*;
 
 public class Application implements Usable {
 
     private Helper helper = new Helper();
-
     private SalesRegistry sr = new SalesRegistry();
-    //Ta i mote filnavn og sende over til Marina sin FileReader
 
     public void start () {
-
-        String filename = helper.getString("Enter CSV.file name");
-
-        // kanskje vi skal ha en annen lese-inn klasse? Ikke Scanner altså?
-
-        SalesReaderCsv filereader = new SalesReaderCsv(sr);
+        String fileName = helper.getString("Enter CSV.file name");
+        SalesReaderCsv fileReader = new SalesReaderCsv(sr);
         try {
-            if(filereader.openFile(filename)) {
-                filereader.readFile();
+            if(fileReader.openFile(fileName)) {
+                fileReader.readFile();
                 menu();
-
             }
-
         } catch (IOException e) {
             System.out.println("Can't find file, sorry pal.");
         }
-
-
     }
 
     public void menu() {
-
-
         int option = -1;
         do {
-
             try {
                 System.out.println("Options");
                 System.out.println("Menu choice 1: Number of orders by region");
@@ -56,30 +40,21 @@ public class Application implements Usable {
                     case 1:
                         System.out.println(sr.getStatisticsAsString(StatisticType.NUMBER_OF_ORDERS_BY_REGION));
                         break;
-
                     case 2:
-
-                        System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
-
+                        //System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
                         break;
                     case 3:
-
-                           System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));;
-
+                        //System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
                         break;
                     case 4:
-
-                            System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
-
+                        //System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
                         break;
                     case 5:
-
-                        System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
-
+                        //System.out.println(sr.getStatisticsAsString(StatisticType.XXXXXXXXXXXXX));
                         break;
                 }
             } catch (Exception e) {
-
+                System.out.printf("Wrong input.", e.getMessage(), e.getCause());
             }
         } while (option != 6);
     }
