d44ed3dce9a4ba1df41bfe967f63048eb8f80bb2
marinahaugen <marina.santos.haugen@dnb.no>
2021-02-25 23:06:52 +0100

    Fixed 2 tests for getAndValidateOptionFromUser

--- src/no/dnb/reskill/assignment/Application.java
+++ src/no/dnb/reskill/assignment/Application.java
@@ -4,7 +4,6 @@ import no.dnb.reskill.assignment.statistics.StatisticType;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.InputMismatchException;
 
 public class Application {
 
